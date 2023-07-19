1f08a37824c13cd7c2b351cdcc9aa30ca80a2cea
Danilo N.B <danilobenatti@gmail.com>
2023-07-19 18:13:37 -0300

    Reversing letters and words

---
+++ src/ReverseLettersAndWords.java
@@ -0,0 +1,46 @@
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+public class ReverseLettersAndWords {
+	
+	/* *
+	 * Chapter 1: Exercise 3 - Reversing letters and words
+	 * Reverse only the letters of each word.
+	 * */
+	public static void main(String[] args) {
+		String str = "Hello World!!!";
+		String reversed1 = reverseWords(str);
+		System.out.printf("\nReversing 1: %s", reversed1);
+		String reversed2 = reverseWords2(str);
+		System.out.printf("\nReversing 2: %s", reversed2);
+		String reversed3 = reverseWords3(str);
+		System.out.printf("\nReversing 3: %s", reversed3);
+	}
+	
+	private static final String WHITESPACE = " ";
+	public static String reverseWords(String str) {
+		
+		String[] words = str.split(WHITESPACE);
+		StringBuilder reversedString = new StringBuilder();
+		
+		for (String word : words) {
+			StringBuilder reverseWord = new StringBuilder();
+			for (int i = word.length() - 1; i >= 0; --i) {
+				reverseWord.append(word.charAt(i));
+			}
+			reversedString.append(reverseWord).append(WHITESPACE);
+		}
+		return reversedString.toString();
+	}
+	
+	private static final Pattern PATTERN = Pattern.compile(" +");
+	
+	public static String reverseWords2(String str) {
+		return PATTERN.splitAsStream(str).map(w -> new StringBuilder(w)
+				.reverse()).collect(Collectors.joining(" "));
+	}
+	
+	public static String reverseWords3(String str) {
+		return new StringBuilder(str).reverse().toString();
+	}
+}
